996a1ca8e1a5d10fae35f24a47d6b6f15dda0276
William Nkosi <william.dev.acc@gmail.com>
2020-10-20 10:56:24 -0500

    changed android package name

--- codemagicpoc/android/app/src/main/kotlin/com/example/codemagicpoc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.codemagicpoc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ codemagicpoc/android/app/src/main/kotlin/io/william/codemagicpoc/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.william.codemagicpoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
